c7fd6ef8bc4702df98a61187e5419f7561e763fa
caleb <lcaleblhenao@gmail.com>
2019-03-12 23:56:04 -0500

    Segundo panel de la interfaz

---
+++ BookStore/src/interfaz/BusquedaController.java
@@ -0,0 +1,15 @@
+package interfaz;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class BusquedaController {
+
+	
+	@FXML private TableView catalogo;
+	@FXML private TableView clientes;
+	@FXML private TableView libroXCliente;
+	
+	
+	
+}
