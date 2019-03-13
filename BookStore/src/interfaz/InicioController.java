cf937ad750e440a6bdc85b37f0b99efb54290068
caleb <lcaleblhenao@gmail.com>
2019-03-12 23:31:39 -0500

    Primer panel de interfaz

--- BookStore/src/interfaz/InicioController.java
+++ BookStore/src/interfaz/InicioController.java
@@ -1,5 +1,26 @@
 package interfaz;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class InicioController {
 	
+	
+	@FXML private TextField numCajeros;
+	@FXML private TextField numClientes;
+	@FXML private Button botonInicio;
+	
+	
+	
+	
+	public InicioController() {}
+	
+	
+	public void initialize() {
+		
+		
+	}
+	
+	
 }
+
