1887d660017f7757bc733c388693f1964214c6a0
caleb <lcaleblhenao@gmail.com>
2019-03-17 14:04:40 -0500

    Segundo panel de información

---
+++ BookStore/src/interfaz/InformationController.java
@@ -0,0 +1,14 @@
+package interfaz;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class InformationController {
+
+	
+	
+	 @FXML private TextArea information;
+	 
+	
+	
+}
